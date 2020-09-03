e356e0550f3701de88b5c3ea976d9a907cef1b99
cdassow <cdassow@nd.edu>
2020-09-03 09:54:05 -0500

    edits to q2 paper outline, almost there I think

--- q2Func.R
+++ q2Func.R
@@ -1,8 +1,9 @@
 #script to hold the q2 function along so we can just source it in Rmarkdown 
 #parameter definitions
 # qE - harvest, species specific, this is what is controlled by 'regulations'
-# s - survial, species specific
-# cJA - effect of adults of a given species on juveniles of a given species (cover cannibalism or interspecific predation)
+# s - juvenile overwinter survial, species specific
+# m - adult natural mortality rate, species specific
+# cJA - effect of adults of a given species on juveniles of a given species (cover cannibalism or interspecific predation, both happen in foraging arena)
 # cJJ - effect of juveniles of one species on juveniles of the other (can be predation or competition)
 # h - rate at which juveniles leave foraging arena for refuge, species specific
 # v - rate at which juveniles enter foraging arena from refuge, species specific
@@ -16,10 +17,10 @@ simBiggsQ2<-function(t,y,params){
   J1<-y[3]
   J2<-y[4]
   with(as.list(params),{
-    dA1dt=-qE1Fun(t)*A1+(s1-1)*A1+s1*J1
-    dA2dt=-qE2Fun(t)*A2+(s2-1)*A2+s2*J2
-    dJ1dt=-cJ1A1*J1*A1-cJ1J2*J2*J1-(cJ1A2*v1*A2*J1)/(h1Fun(t)+v1+cJ1A2*A2)+f1*A1+st1Fun(t)
-    dJ2dt=-cJ2A2*J2*A2-cJ2J1*J1*J2-(cJ2A1*v2*A1*J2)/(h2Fun(t)+v2+cJ2A1*A1)+f2*A2+st2Fun(t)
+    dA1dt=-qE1Fun(t)*A1+m1*A1+s1*J1
+    dA2dt=-qE2Fun(t)*A2+m2*A2+s2*J2
+    dJ1dt=-cJ1J2*J2*J1-((cJ1A2*v1*A2*J1)/(h1Fun(t)+v1+cJ1A2*A2))-((cJ1A1*v1*A1*J1)/(h1Fun(t)+v1+cJ1A1*A1))+f1*A1+st1Fun(t)
+    dJ2dt=-cJ2J1*J1*J2-((cJ2A1*v2*A1*J2)/(h2Fun(t)+v2+cJ2A1*A1))-((cJ2A2*v2*A2*J2)/(h2Fun(t)+v2+cJ2A2*A2))+f2*A2+st2Fun(t)
     return(list(c(dA1dt,dA2dt,dJ1dt,dJ2dt)))
   })
 }
