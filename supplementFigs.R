8f6bced463d1260a68dde1c5a4a89de34e648d1b
cdassow <cdassow@nd.edu>
2020-12-22 15:26:28 -0600

    cleaned up supplemental fig script and made plots match manuscript

--- supplementFigs.R
+++ supplementFigs.R
@@ -16,8 +16,15 @@ y0=c(100,10,0,0)
 p=c(s1=0.1,m1=0.5,cJ1A1=0.001,cJ1A2=0.5,cJ1J2=0.003,v1=1,
     s2=0.1,m2=0.5,cJ2A2=0.001,cJ2A1=0.3,cJ2J1=0.003,v2=1)
 sim=ode(y=y0,times=tstep,func=simBiggsQ2,parms=p)
-plot(sim[,1],sim[,2],type = 'l',lwd=2,ylim = c(0,max(sim[,2:3],na.rm = T)))
-lines(sim[,1],sim[,3],col='gray',lwd=2)
+sf1=as.data.frame(rbind(cbind(sim[,c(1,2)],rep("A1",nrow(sim))),cbind(sim[,c(1,3)],rep("A2",nrow(sim)))))
+colnames(sf1)=c("Time","Abund","sp")
+sf1$Abund=as.numeric(sf1$Abund);sf1$Time=as.numeric(sf1$Time)
+
+figS1=ggplot(data = sf1,aes(x=Time,y=Abund,color=sp))+theme_classic()+
+  geom_line(size=1)+scale_color_manual(values = c("black","grey"),name="")+
+  labs(x='Time',y='Adult Abundance')+
+  theme(legend.position = 'bottom')
+figS1
 
 
 #same thing to demonstrate the effect of refuge loss, describe in methods, fig may go in supplement
@@ -34,8 +41,15 @@ y0=c(100,10,0,0)
 p=c(s1=0.1,m1=0.5,cJ1A1=0.001,cJ1A2=0.5,cJ1J2=0.003,v1=1,
     s2=0.1,m2=0.5,cJ2A2=0.001,cJ2A1=0.3,cJ2J1=0.003,v2=1)
 sim=ode(y=y0,times=tstep,func=simBiggsQ2,parms=p)
-plot(sim[,1],sim[,2],type = 'l',lwd=2,ylim = c(0,max(sim[,2:3],na.rm = T)))
-lines(sim[,1],sim[,3],col='gray',lwd=2)
+sf2=as.data.frame(rbind(cbind(sim[,c(1,2)],rep("A1",nrow(sim))),cbind(sim[,c(1,3)],rep("A2",nrow(sim)))))
+colnames(sf2)=c("Time","Abund","sp")
+sf2$Abund=as.numeric(sf2$Abund);sf2$Time=as.numeric(sf2$Time)
+
+figS2=ggplot(data = sf2,aes(x=Time,y=Abund,color=sp))+theme_classic()+
+  geom_line(size=1)+scale_color_manual(values = c("black","grey"),name="")+
+  labs(x='Time',y='Adult Abundance')+
+  theme(legend.position = 'bottom')
+figS2
 
 
 #flip scenarios
@@ -105,10 +119,52 @@ for(i in 1:nrow(dfwo2)){
 
 dfwo2$diff=dfwo2$A1-dfwo2$A2
 dfwo2$outcome=ifelse(dfwo2$A1-dfwo2$A2 < minDiff,"darkgreen", "darkred")
-
+df2$mod=rep("Flip to 1, ignore 2",nrow(df2))
+dfwo2$mod=rep("Flip to 1, harv 2",nrow(dfwo2))
 allSen=rbind(df2,dfwo2)
 vzI=ggplot(data=allSen, aes(x=allSen$X,y=allSen$Y,linetype=allSen$mod))+theme_classic()+
   geom_contour(aes(z=allSen$diff),breaks = c(minDiff), color='black', size=2)+
   labs(x="Species 1 Harvest Rate", y="Species 1 Stocking",linetype="Scenario")+
   theme(legend.position = 'bottom')
 vzI
+
+#plot to look at the cost/benefits of stocking or predator reduction for managing a focal species. Flip scenario instead of maintain scenario 
+
+minDiff=100
+qEs=rep(seq(0,8,length.out = 30),3)
+sto=rep(seq(0,2000, length.out = 30),3)
+dfT=expand.grid(X=qEs,Y=sto)
+dfT$A1=numeric(nrow(dfT))
+dfT$A2=numeric(nrow(dfT))
+dfT$J1=numeric(nrow(dfT))
+dfT$J2=numeric(nrow(dfT))
+dfT$sp1H=c(rep(2,30),rep(4,30),rep(6,30))
+
+for(i in 1:nrow(dfT)){
+  tstep=1:100
+  qE1Fun=approxfun(x=tstep,y=rep(dfT$sp1H[i], length(tstep)))
+  qE2Fun=approxfun(x=tstep,y=rep(dfT$X[i], length(tstep)))
+  st1Fun=approxfun(x=tstep,y=rep(dfT$Y[i],length(tstep)))
+  st2Fun=approxfun(x=tstep,y=rep(0,length(tstep)))
+  p=c(s1=0.1,m1=0.5,cJ1A1=0.002,cJ1A2=0.5,cJ1J2=0.003,v1=1,
+      s2=0.1,m2=0.5,cJ2A2=0.002,cJ2A1=0.3,cJ2J1=0.003,v2=1)
+  y0=c(100,1000,0,0)
+  sim=ode(y=y0,times=tstep,func=simBiggsQ2,parms=p)
+  dfT$A1[i]=sim[nrow(sim)-1,2]
+  dfT$A2[i]=sim[nrow(sim)-1,3]
+  dfT$J1[i]=sim[nrow(sim)-1,4]
+  dfT$J2[i]=sim[nrow(sim)-1,5]
+}
+
+dfT$diff=dfT$A1-dfT$A2
+dfT$qNorm=(dfT$X-min(dfT$X))/(max(dfT$X)-min(dfT$X))
+dfT$sNorm=(dfT$Y-min(dfT$Y))/(max(dfT$Y)-min(dfT$Y))
+dfT$sp1Norm=((dfT$sp1H-min(dfT$X))/(max(dfT$X)-min(dfT$X))) #putting sp1 harv on same scale as sp2
+
+vzT=ggplot(data=dfT, aes(x=dfT$X,y=dfT$Y,linetype=as.factor(dfT$sp1H)))+theme_classic()+
+  geom_contour(aes(z=dfT$diff),breaks = c(minDiff), color='black', size=1)+
+  labs(x="Species 2 Harvest Rate", y="Species 1 Stocking",linetype="Species 1 Harvest")+
+  theme(legend.position = 'bottom')+
+  xlim(0,8)+
+  ylim(0,2000)
+vzT
