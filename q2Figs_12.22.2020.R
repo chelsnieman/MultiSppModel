87d353758a300683f785725db720fd1f31f7ca88
cdassow <cdassow@nd.edu>
2021-01-01 15:42:16 -0500

    Colin finished work on draft and supplement

--- q2Figs_12.22.2020.R
+++ q2Figs_12.22.2020.R
@@ -1,4 +1,4 @@
-## code to think about new ways to vizualise some of our results
+## code to think about new ways to visualize some of our results
 
 rm(list=ls())
 library(deSolve)
@@ -228,79 +228,109 @@ vzT=ggplot(data=dfT, aes(x=dfT$X,y=dfT$Y,linetype=as.factor(dfT$sp1H)))+theme_cl
   ylim(0,2000)
 vzT
 
-#figure on delaying a transition
+#### DELAY A TRANSITION ####
+#same function as used in other figs but with option to change fecundity now
+#comes from 'fecundityDecline.R' script
+simBiggsR.a<-function(t,y,params){
+  A1<-y[1]
+  A2<-y[2]
+  J1<-y[3]
+  J2<-y[4]
+  with(as.list(params),{
+    dA1dt=-qE1Fun(t)*A1-m1*A1+s1*J1
+    dA2dt=-qE2Fun(t)*A2-m2*A2+s2*J2
+    dJ1dt=-cJ1J2*J2*J1-((cJ1A2*v1*A2*J1)/(h1Fun(t)+v1+cJ1A2*A2))-((cJ1A1*v1*A1*J1)/(h1Fun(t)+v1+cJ1A1*A1))-s1*J1+((5000*dFun(t))*A1/(500+A1))+st1Fun(t)
+    dJ2dt=-cJ2J1*J1*J2-((cJ2A1*v2*A1*J2)/(h2Fun(t)+v2+cJ2A1*A1))-((cJ2A2*v2*A2*J2)/(h2Fun(t)+v2+cJ2A2*A2))-s2*J2+(5000*A2/(500+A2))+st2Fun(t)
+    return(list(c(dA1dt,dA2dt,dJ1dt,dJ2dt)))
+  })
+}
 
 #running to sp1 dominating before habitat decline
 tstep=1:200
-h1Fun=approxfun(x=tstep,y=c(rep(10,200)))
-h2Fun=approxfun(x=tstep,y=c(rep(10,200)))
+h1Fun=approxfun(x=tstep,y=c(rep(8,200)))
+h2Fun=approxfun(x=tstep,y=c(rep(8,200)))
 qE1Fun=approxfun(x=tstep,y=c(rep(0,length(tstep))))
 qE2Fun=approxfun(x=tstep,y=c(rep(0,length(tstep))))
 st1Fun=approxfun(x=tstep,y=c(rep(0,length(tstep))))
 st2Fun=approxfun(x=tstep,y=rep(0,length(tstep)))
-p=c(s1=0.1,m1=0.5,cJ1A1=0.002,cJ1A2=0.5,cJ1J2=0.003,v1=1,
-    s2=0.1,m2=0.5,cJ2A2=0.002,cJ2A1=0.3,cJ2J1=0.003,v2=1)
-y0=c(500,100,0,0)
-simPre=ode(y=y0,times=tstep,func=simBiggsQ2,parms=p)
+dFun=approxfun(x=tstep,y=rep(1,length(tstep)))
 
-#habitat decline and stocking
+p=c(s1=0.1,m1=0.5,cJ1A1=0.001,cJ1A2=0.5,cJ1J2=0.003,v1=1,
+    s2=0.1,m2=0.5,cJ2A2=0.001,cJ2A1=0.3,cJ2J1=0.003,v2=1)
+y0=c(500,100,0,0)
+simPre=ode(y=y0,times=tstep,func=simBiggsR.a,parms=p)
+
+#Fecundity decline -> what will happen if no action taken
 tstep=1:500
-h1Fun=approxfun(x=tstep,y=c(rep(10,100),seq(10,0, length.out = 100), rep(0,300)))
-h2Fun=approxfun(x=tstep,y=c(rep(10,100),seq(10,10, length.out = 100),rep(10,300)))
-qE1Fun=approxfun(x=tstep,y=c(rep(0,100), seq(0,4,length.out = 200), rep(4,200)))
+h1Fun=approxfun(x=tstep,y=c(rep(8,500)))
+h2Fun=approxfun(x=tstep,y=c(rep(8,500)))
+qE1Fun=approxfun(x=tstep,y=c(rep(0,500)))
 qE2Fun=approxfun(x=tstep,y=c(rep(0,500)))
 st1Fun=approxfun(x=tstep,y=c(rep(0,500)))
-st2Fun=approxfun(x=tstep,y=c(rep(4,500)))#this is to simulate increase in fecundity, could change the model to make f time dependent, or have some amount of harvest that declines to 0
+st2Fun=approxfun(x=tstep,y=c(rep(0,500)))
+dFun=approxfun(x=tstep,y=c(seq(1,.01,length.out = 200),rep(.01,300)))
+
 p=c(s1=0.1,m1=0.5,cJ1A1=0.002,cJ1A2=0.5,cJ1J2=0.003,v1=1,
     s2=0.1,m2=0.5,cJ2A2=0.002,cJ2A1=0.3,cJ2J1=0.003,v2=1)
 y0=simPre[199,2:5]
-sim=ode(y=y0,times=tstep,func=simBiggsQ2,parms=p)
+sim=ode(y=y0,times=tstep,func=simBiggsR.a,parms=p)
 
 #stocking delays the transition to sp2
 tstep=1:500
-h1Fun=approxfun(x=tstep,y=c(rep(10,100),seq(10,0, length.out = 100), rep(0,300)))
-h2Fun=approxfun(x=tstep,y=c(rep(10,100),seq(10,10, length.out = 100),rep(10,300)))
-qE1Fun=approxfun(x=tstep,y=c(rep(0,100), seq(0,4,length.out = 200), rep(4,200)))
+h1Fun=approxfun(x=tstep,y=c(rep(8,500)))
+h2Fun=approxfun(x=tstep,y=c(rep(8,500)))
+qE1Fun=approxfun(x=tstep,y=c(rep(0,500)))
 qE2Fun=approxfun(x=tstep,y=c(rep(0,500)))
-st1Fun=approxfun(x=tstep,y=c(rep(0,100),rep(80,400)))
-st2Fun=approxfun(x=tstep,y=c(rep(4,500))) #this is to simulate increase in fecundity, could change the model to make f time dependent, or have some amount of harvest that declines to 0
+st1Fun=approxfun(x=tstep,y=c(rep(0,100),rep(10,400)))
+st2Fun=approxfun(x=tstep,y=c(rep(0,500))) 
+dFun=approxfun(x=tstep,y=c(seq(1,.01,length.out = 200),rep(.01,300)))
 
-simS=ode(y=y0,times=tstep,func=simBiggsQ2,parms=p)
+simS=ode(y=y0,times=tstep,func=simBiggsR.a,parms=p)
 
-#harvesting delays the transition to sp2
+#harvesting delays the transition to sp2 - no stocking
 tstep=1:500
-h1Fun=approxfun(x=tstep,y=c(rep(10,100),seq(10,0,length.out = 100), rep(0,300)))
-h2Fun=approxfun(x=tstep,y=c(rep(10,100),seq(10,10,length.out = 100),rep(10,300)))
-qE1Fun=approxfun(x=tstep,y=c(rep(0,100), seq(0,4,length.out = 200), rep(4,200)))
-qE2Fun=approxfun(x=tstep,y=c(rep(0,100), seq(0,.5,length.out = 200), rep(.5,200)))
+h1Fun=approxfun(x=tstep,y=c(rep(8,500)))
+h2Fun=approxfun(x=tstep,y=c(rep(8,500)))
+qE1Fun=approxfun(x=tstep,y=c(rep(0,500)))
+qE2Fun=approxfun(x=tstep,y=c(rep(0,100), seq(0,.75,length.out = 200), rep(.75,200)))
 st1Fun=approxfun(x=tstep,y=c(rep(0,500)))
-st2Fun=approxfun(x=tstep,y=c(rep(5,500))) #this is to simulate increase in fecundity, could change the model to make f time dependent, or have some amount of harvest that declines to 0
+st2Fun=approxfun(x=tstep,y=c(rep(0,500))) 
+dFun=approxfun(x=tstep,y=c(seq(1,.01,length.out = 200),rep(.01,300)))
 
-simH=ode(y=y0,times=tstep,func=simBiggsQ2,parms=p)
+simH=ode(y=y0,times=tstep,func=simBiggsR.a,parms=p)
 
 #harvesting and stocking delays the transition to sp2
 tstep=1:500
-h1Fun=approxfun(x=tstep,y=c(rep(10,100),seq(10,0,length.out = 100), rep(0,300)))
-h2Fun=approxfun(x=tstep,y=c(rep(10,100),seq(10,10,length.out = 100),rep(10,300)))
-qE1Fun=approxfun(x=tstep,y=c(rep(0,100), seq(0,4,length.out = 200), rep(4,200)))
-qE2Fun=approxfun(x=tstep,y=c(rep(0,100), seq(0,.5,length.out = 200), rep(.5,200)))
-st1Fun=approxfun(x=tstep,y=c(rep(0,100),rep(80,400)))
-st2Fun=approxfun(x=tstep,y=c(rep(5,500))) #this is to simulate increase in fecundity, could change the model to make f time dependent, or have some amount of harvest that declines to 0
+h1Fun=approxfun(x=tstep,y=c(rep(8,500)))
+h2Fun=approxfun(x=tstep,y=c(rep(8,500)))
+qE1Fun=approxfun(x=tstep,y=c(rep(0,500)))
+qE2Fun=approxfun(x=tstep,y=c(rep(0,100), seq(0,.75,length.out = 200), rep(.75,200)))
+st1Fun=approxfun(x=tstep,y=c(rep(0,100),rep(15,400)))
+st2Fun=approxfun(x=tstep,y=c(rep(0,500))) 
+dFun=approxfun(x=tstep,y=c(seq(1,.01,length.out = 200),rep(.01,300)))
 
-simB=ode(y=y0,times=tstep,func=simBiggsQ2,parms=p)
+simB=ode(y=y0,times=tstep,func=simBiggsR.a,parms=p)
 
 #plotting
 
 # ggplot version
+
+#do nothing
 panA=as.data.frame(rbind(cbind(sim[,c(1,2)],rep("A1",nrow(sim))),cbind(sim[,c(1,3)],rep("A2",nrow(sim)))))
 colnames(panA)=c("Time","Abund","sp")
 panA$Abund=as.numeric(panA$Abund);panA$Time=as.numeric(panA$Time)
+
+#harvest sp2
 panB=as.data.frame(rbind(cbind(simH[,c(1,2)],rep("A1",nrow(simH))),cbind(simH[,c(1,3)],rep("A2",nrow(simH)))))
 colnames(panB)=c("Time","Abund","sp")
 panB$Time=as.numeric(panB$Time);panB$Abund=as.numeric(panB$Abund)
+
+#stock sp1
 panC=as.data.frame(rbind(cbind(simS[,c(1,2)],rep("A1",nrow(simS))),cbind(simS[,c(1,3)],rep("A2",nrow(simS)))))
 colnames(panC)=c("Time","Abund","sp")
 panC$Abund=as.numeric(panC$Abund);panC$Time=as.numeric(panC$Time)
+
+#stock and harvest
 panD=as.data.frame(rbind(cbind(simB[,c(1,2)],rep("A1",nrow(simB))),cbind(simB[,c(1,3)],rep("A2",nrow(simB)))))
 colnames(panD)=c("Time","Abund","sp")
 panD$Abund=as.numeric(panD$Abund);panD$Time=as.numeric(panD$Time)
@@ -310,27 +340,24 @@ a4=ggplot(data = panA,aes(x=Time,y=Abund,color=sp))+theme_classic()+
   theme(axis.title.x = element_blank(),
         axis.text.x = element_blank(),
         axis.title.y = element_blank())
-a4
 b4=ggplot(data = panB,aes(x=Time,y=Abund,color=sp))+theme_classic()+
   geom_line(size=1)+scale_color_manual(values = c("black","grey"),name="")+
   theme(axis.title.x = element_blank(),
         axis.text.x = element_blank(),
         axis.title.y = element_blank(),
         axis.text.y = element_blank())
-b4
 c4=ggplot(data = panC,aes(x=Time,y=Abund,color=sp))+theme_classic()+
   geom_line(size=1)+scale_color_manual(values = c("black","grey"),name="")+
   theme(axis.title.x = element_blank(),
         axis.title.y = element_blank())
-c4
 d4=ggplot(data = panD,aes(x=Time,y=Abund,color=sp))+theme_classic()+
   geom_line(size=1)+scale_color_manual(values = c("black","grey"),name="")+
   theme(axis.title.x = element_blank(),
         axis.title.y = element_blank(),
         axis.text.y = element_blank())
-d4
 
 fig4=ggarrange(a4,b4,c4,d4,labels = c("A","B","C","D"),common.legend = T,legend = "top",label.x = .8,label.y = .9)
 annotate_figure(fig4,
                 left = text_grob("Adult Abundance", rot = 90),
                 bottom = text_grob("Time"))
+
